1ba5e30d9f25e5afe7264a1c21a3fe876434fb4e
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-21 11:44:44 -0700

    Add button, js  <<Co-authored-by:Tyler Sinks <tyler.sinks@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#form_one").submit(function (event) {
+    const firstName = $("input#first_name").val();
+    const lastName = $("input#last_name").val();
+    $(".person").text(firstName);
+    $(".person").append(" ");
+    $(".person").append(lastName);
+    event.preventDefault();
+  })
+});
