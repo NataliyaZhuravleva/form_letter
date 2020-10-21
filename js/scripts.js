4c7bcd29620ecdfd0f4c8ff4d418b283d8482e9e
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-21 13:13:21 -0700

    Final version  <<Co-authored-by:Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
   $("#form_one").submit(function (event) {
     const firstName = $("input#first_name").val();
     const lastName = $("input#last_name").val();
+    
     $(".person").text(firstName);
     $(".person").append(" ");
     $(".person").append(lastName);
+    $(".hidden").show();
     event.preventDefault();
   })
 });
